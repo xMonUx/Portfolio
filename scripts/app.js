54130106d4fe4748d4effd1976b175ff41814e4b
xMonUx <infinityfor0m@gmail.com>
2023-03-19 07:53:01 +0100

    Dodany fixed navbar, footer i poprawione animacje

--- scripts/app.js
+++ scripts/app.js
@@ -6,21 +6,54 @@ $(window).scroll(function(){
     }
 });
 
-$(document).ready(function() {
+// $(document).ready(function() {
   
-    var counter = 0;
-    var c = 0;
-    var i = setInterval(function(){
-        $(".loader .counter h1").html(c + "%");
-        $(".loader .counter hr").css("width", c + "%");
+//     var counter = 0;
+//     var c = 0;
+//     var i = setInterval(function(){
+//         $(".loader .counter h1").html(c + "%");
+//         $(".loader .counter hr").css("width", c + "%");
 
-      counter++;
-      c++;
+//       counter++;
+//       c++;
         
-      if(counter == 101) {
-          clearInterval(i);
-          $('body').addClass('loaded');
-          $('.loader').fadeOut();
-      }
-    }, 10);
-  });
+//       if(counter == 101) {
+//           clearInterval(i);
+//           $(".loader").fadeOut("slow");
+//           setTimeout(function() {
+//               $("body").addClass("loaded");
+//           }, 500);
+//       }
+//     }, 10);
+//   });
+
+
+window.console = window.console || function(t) {};
+  
+  if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+  }
+  
+  (function($) {
+    "use strict";
+  
+    var $navbar = $(".navbar"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 350) {
+          $navbar.addClass("fixed");
+        } else {
+          $navbar.removeClass("fixed");  
+        }
+    });
+
+})(jQuery, undefined);
+
+$(".menu").click(function(){
+  $("#nav").toggleClass("open");
+});
+
+
