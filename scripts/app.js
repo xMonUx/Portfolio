30079d9633ef8c02d872c5e6b8218205ddaeabe3
xMonUx <infinityfor0m@gmail.com>
2023-03-21 00:02:46 +0100

    Dodano sekcje projects

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,39 @@
-// Navbar unhide after 400px
-  // $(window).on('scroll', function() {
-  //   if ($(this).scrollTop() > 400) {
-  //     $('.content__navbar').removeClass('content__navbar--hide');
-  //   } else {
-  //     $('.content__navbar').addClass('content__navbar--hide');
-  //   }
-  // });
+  $(window).on('scroll', function() {
+    if ($(this).scrollTop() > 400) {
+      $('.content__navbar').removeClass('content__navbar--hide');
+    } else {
+      $('.content__navbar').addClass('content__navbar--hide');
+    }
+  });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var pr = document.querySelector('.arrows__arrow--left');
+  var pl = document.querySelector('.arrows__arrow--right');
+
+  if (pr) {
+    pr.onclick = slide.bind(this, -1);
+  }
+
+  if (pl) {
+    pl.onclick = slide.bind(this, 1);
+  }
+
+  var index = 0, total = 3; //liczba stron
+
+  function slide(offset) {
+    index = Math.min(Math.max(index + offset, 0), total -1);
+
+    document.querySelectorAll('.arrows__counter').innerHTML = (index + 1) + '/' + total;
+
+    if (pr) {
+      pr.setAttribute( 'data-state', index === 0 ? 'disabled' : '' );
+    }
+
+    if (pl) {
+      pl.setAttribute( 'data-state', index === total - 1 ? 'disabled' : '' );
+    }
+  }
+
+  slide(0);
+});
