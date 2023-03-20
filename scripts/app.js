23de592a850e70056c5df744448bc90c676af8d2
xMonUx <infinityfor0m@gmail.com>
2023-03-20 20:46:12 +0100

    Rebuild strony, start od ustawienia gridu i responsywności

--- scripts/app.js
+++ scripts/app.js
@@ -1,59 +1,8 @@
-$(window).scroll(function(){
-    if($(this).scrollTop() > 200){
-        $('.hide-socials').stop().animate({opacity: 0}, 200);
-    } else {
-        $('.hide-socials').stop().animate({opacity: 1}, 200);
-    }
-});
-
-// $(document).ready(function() {
-  
-//     var counter = 0;
-//     var c = 0;
-//     var i = setInterval(function(){
-//         $(".loader .counter h1").html(c + "%");
-//         $(".loader .counter hr").css("width", c + "%");
-
-//       counter++;
-//       c++;
-        
-//       if(counter == 101) {
-//           clearInterval(i);
-//           $(".loader").fadeOut("slow");
-//           setTimeout(function() {
-//               $("body").addClass("loaded");
-//           }, 500);
-//       }
-//     }, 10);
-//   });
-
-
-window.console = window.console || function(t) {};
-  
-  if (document.location.search.match(/type=embed/gi)) {
-    window.parent.postMessage("resize", "*");
-  }
-  
-  (function($) {
-    "use strict";
-  
-    var $navbar = $(".navbar"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
-
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 350) {
-          $navbar.addClass("fixed");
-        } else {
-          $navbar.removeClass("fixed");  
-        }
-    });
-
-})(jQuery, undefined);
-
-$(".menu").click(function(){
-  $("#nav").toggleClass("open");
-});
-
-
+// Navbar unhide after 400px
+  // $(window).on('scroll', function() {
+  //   if ($(this).scrollTop() > 400) {
+  //     $('.content__navbar').removeClass('content__navbar--hide');
+  //   } else {
+  //     $('.content__navbar').addClass('content__navbar--hide');
+  //   }
+  // });
